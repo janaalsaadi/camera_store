f3e1bd71bfa2374ccdb84b2cdd6b5d9d0f18ed84
janaalsaadi <47085679+janaalsaadi@users.noreply.github.com>
2020-10-05 11:55:31 +0300

    jana

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React ,{Component}from 'react';
-import Clock from './components/Clock';
-import Header from './components/Header';
+import Clock from './components/clock/Clock';
+import Header from './components/header/Header';
 class App extends React.Component{
 
   render(){
